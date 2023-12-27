39350139c88681bf5169e07b45330b2bfab6faa0
Daniel Unknown <danielunknown@Daniels-MBP.attlocal.net>
2023-12-26 21:49:30 -0500

    added cart

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+
+// navbar 
 let menu_icon = document.querySelector('.menu-icon');
 let close_icon = document.querySelector('.close-icon');
 let mobile_nav = document.querySelector('.mobile-nav');
@@ -9,3 +12,13 @@ menu_icon.addEventListener('click', ()=>{
 close_icon.addEventListener('click', ()=>{
     mobile_nav.classList.remove('nav-open')
 })
+
+
+// cart
+let cart_icon = document.querySelector('.cart-icon');
+let cart_items = document.querySelector('.cart-items');
+
+cart_icon.addEventListener('click', ()=>{
+    cart_items.classList.toggle('cart-open')
+})
+
